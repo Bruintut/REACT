4921b62c41efcb6552c30d5f97fe46d5aa974d6d
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-24 12:56:52 -0300

    feat: add config Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
+import '../Navbar/Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <h2>
-            
+            <Link to={`/`}>Blog</Link>
         </h2>
+        <ul>
+            <li>
+                <Link to={`/`}>Inicio</Link>
+            </li>
+            <li>
+                <Link to={`/new`} className='new-btn'>Novo Post</Link>
+            </li>
+        </ul>
     </nav>
   )
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,13 @@
+import Navbar from "components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+
 import "./Home.css"
 
 function Home(){
 
   return(
     <div className="Home">
+      <Navbar/>
       <div className="container">
         <Outlet/>
 
